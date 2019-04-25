2dac35172e424d97554f7abc4df6a9dedb942550
T-Rekt <t.to.the.rekt@gmail.com>
2019-04-25 12:00:28 +0700

    Fucking trying the example to make it works

--- api/getFeed.js
+++ api/getFeed.js
@@ -1,12 +1,7 @@
 const { getFeed } = require('tiktokk-api');
 const { json } = require('micro');
 
-module.exports = async(req, res) => {
-  try {
-    const data = await json(req);
-    return res.send(await getFeed(data), 200);
-  } catch (e) {
-    console.error(e.message);
-    return res.send(e, 400);
-  }
+module.exports = async(req) => {
+  const data = await json(req);
+  return await getFeed(data);
 }
